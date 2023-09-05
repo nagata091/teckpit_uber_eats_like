abd6b636f88d3fe82a293d51010d452b2a67fa5a
nagata091 <nagata.kz091@gmail.com>
2023-09-05 18:42:10 +0900

    : +1: オーダーコントローラーを作成

---
+++ app/controllers/api/v1/Orders_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::OrdersController < ApplicationController
+
+  def create
+    # 仮注文の一覧を取得する
+    posted_line_foods = LineFood.where(id: params[:line_food_ids])
+    # 本注文を作成する
+    order = Order.new(
+      total_price: total_price(posted_line_foods),
+    )
+    # 本注文の保存に成功した場合は、仮注文の情報を返す
+    if order.save_with_update_line_foods!(posted_line_foods)
+      render json: {}, status: :no_content
+    # 本注文の保存に失敗した場合は、エラーを返す
+    else
+      render json: {}, status: :internal_server_error
+    end
+  end
+
+
+  private
+
+  # 本注文の合計金額を計算する
+  def total_price
+    # 本注文の合計金額 = 仮注文の合計金額 + レストランの手数料
+    posted_line_foods.sum { |line_food| line_food.total_amount } + posted_line_foods.first.restaurant.fee
+  end
+end
