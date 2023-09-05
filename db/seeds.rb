f22ff600f83f12b0ab60f5e6ad1aab50d6c18273
nagata091 <nagata.kz091@gmail.com>
2023-09-05 16:55:24 +0900

    :+1: seedでデモ用データを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# デモ用レストランを3つ作成する
+3.times do |n|
+  restaurant = Restaurant.new(
+    name: "おためしレストラン_#{n + 1}",
+    fee: 100,
+    time_required: 10)
+
+    # 1つのレストランあたり12個のデモ用フードを作成する
+    12.times do |m|
+      restaurant.foods.build(
+        name: "おためしフード_#{m + 1}",
+        price: 500,
+        description: "おためしフード_#{m + 1}の説明文です。")
+    end
+
+    # 保存する
+    restaurant.save!
+end
