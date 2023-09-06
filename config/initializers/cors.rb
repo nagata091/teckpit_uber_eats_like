5bf4b655df7af1a03f38838eed12e07cfc9f523a
nagata091 <nagata.kz091@gmail.com>
2023-09-06 19:10:20 +0900

    :+1: コンポーネントからAPIを呼び出せるようにした、CORSを有効にした

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # どのオリジンからのリクエストを許可するか(正規表現でも設定できる)
+    origins "http://localhost:3001"
+
+    # どのようなリクエストを許可するか
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,7 +1,22 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
+
+// apisコンポーネント
+import { fetchRestaurants } from "../apis/restaurants";
 
 
 export const Restaurants = () => {
+
+  // useEffectを使って、コンポーネントがマウントされた時に実行される処理を記述
+  // 第2引数に空の配列[]を渡すことで、コンポーネントのレンダリング時に一度だけ実行される
+  useEffect(() => {
+    // レストラン一覧をAPIを叩いて取得
+    fetchRestaurants()
+    // 取得に成功したら、コンソールに出力
+    .then(
+      (data) => console.log(data)
+    )
+  }, [])　// 第2引数に空の配列
+
   return (
     <Fragment>
       レストラン一覧
