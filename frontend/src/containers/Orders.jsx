53d25218900145dad1f37499379153895404c59a
nagata091 <nagata.kz091@gmail.com>
2023-09-06 17:14:03 +0900

    :+1: react routerを実装

---
+++ frontend/src/containers/Orders.jsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+
+
+export const Orders = () => {
+  return (
+    <Fragment>
+      注文画面
+    </Fragment>
+    )
+}
---
+++ frontend/src/containers/Restaurants.jsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+
+
+export const Restaurants = () => {
+  return (
+    <Fragment>
+      レストラン一覧
+    </Fragment>
+    )
+}
