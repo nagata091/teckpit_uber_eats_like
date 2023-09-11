da79b62ea99bd102f226946b9a5137169f35ec2b
nagata091 <nagata.kz091@gmail.com>
2023-09-11 18:30:40 +0900

    :+1: APIから仮注文データーを取得できるようにした

--- frontend/src/containers/Orders.jsx
+++ frontend/src/containers/Orders.jsx
@@ -1,7 +1,32 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useReducer } from "react";
+import { fetchLineFoods } from "../apis/line_foods";
+
+import {
+  initialState,
+  lineFoodsActionTypes,
+  lineFoodsReducer,
+} from "../reducers/lineFoods";
 
 
 export const Orders = () => {
+
+  const [state, dispatch] = useReducer(lineFoodsReducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: lineFoodsActionTypes.FETCHING });
+    fetchLineFoods()
+      .then((data) =>
+        dispatch({
+          type: lineFoodsActionTypes.FETCH_SUCCESS,
+          payload: {
+            lineFoodsSummary: data
+          }
+        })
+      )
+      // line_foods.jsからthrowされたエラー(e)はここで受け取る
+      .catch((e) => console.error(e))
+  }, []);
+
   return (
     <Fragment>
       注文画面
---
+++ frontend/src/reducers/lineFoods.js
@@ -0,0 +1,45 @@
+import { REQUEST_STATE } from '../constants';
+
+export const initialState = {
+  fetchState: REQUEST_STATE.INITIAL, // 取得状況
+  postState: REQUEST_STATE.INITIAL,  // 登録状況
+  lineFoodsSummary: null,            // 仮注文データ
+};
+
+export const lineFoodsActionTypes = {
+  FETCHING: 'FETCHING',
+  FETCH_SUCCESS: 'FETCH_SUCCESS',
+  POSTING: 'POSTING',
+  POST_SUCCESS: 'POST_SUCCESS',
+}
+
+export const lineFoodsReducer = (state, action) => {
+  switch (action.type) {
+    // 仮注文データを取得中
+    case lineFoodsActionTypes.FETCHING:
+      return {
+        ...state,
+        fetchState: REQUEST_STATE.LOADING,
+      };
+    // 仮注文データを取得完了
+    case lineFoodsActionTypes.FETCH_SUCCESS:
+      return {
+        fetchState: REQUEST_STATE.OK,
+        lineFoodsSummary: action.payload.lineFoodsSummary,
+      };
+    // 仮注文データを登録中
+    case lineFoodsActionTypes.POSTING:
+      return {
+        ...state,
+        postState: REQUEST_STATE.LOADING,
+      };
+    // 仮注文データを登録完了
+    case lineFoodsActionTypes.POST_SUCCESS:
+      return {
+        ...state,
+        postState: REQUEST_STATE.OK,
+      };
+    default:
+      throw new Error();
+  }
+}
